75f78d0136db29ec2980e7a71131fd7d40593608
GABRIELA SOUSA <gabiessx@gmail.com>
2025-02-13 19:27:42 -0300

    Create index.php
    primeiro commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="port-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css.css">
+</head>
+<body>
+    <header class="cabecalho">
+        <h2>Sistema de Controle de Faltas</h2>
+    </header>
+
+    <aside>
+        <ul type="circle">
+            <li>Home</li>
+            <li>Usuário</li>
+            <li>Configurações</li>
+        </ul>
+    </aside>
+
+    <header>
+    </header>
+
+    <main>
+        <h3>Área de trabalho</h3>
+    </main>
+
+    <footer>
+        <p>&copy 2023. Todos os direitos reservados.</p>
+    </footer>
+</body>
+</html>
